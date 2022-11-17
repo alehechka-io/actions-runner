04376f04afa8da63dbfd306b953b7d5b859a9448
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-11-16 21:46:49 -0600

    remove tab

--- controller-manager.secret.sh
+++ controller-manager.secret.sh
@@ -7,4 +7,3 @@ kubectl create secret generic controller-manager \
     --from-literal=github_app_id=${APP_ID} \
     --from-literal=github_app_installation_id=${INSTALLATION_ID} \
     --from-file=github_app_private_key=${PRIVATE_KEY_FILE_PATH}
-    
